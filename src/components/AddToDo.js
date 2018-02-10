4f407403afe21c318117f2d98bb1c6a00beda66a
Stephen Kawaguchi <kawaguchistephen@johndeere.com>
2018-02-10 17:12:47 -0500

    feat: add to do component
    - a stateful component to essentially passes off the to do description and reset its input once a new to do has been added

---
+++ src/components/AddToDo.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+
+function addToDoHandler() {
+    if (this.state.description) {
+        this.props.onClick(this.state.description);
+        this.setState({
+            description: ''
+        });
+    }
+}
+
+function changeDescriptionHandler(event) {
+    this.setState({
+        description: event.target.value
+    });
+}
+
+export class AddToDo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: ''
+        };
+    }
+
+    render() {
+        return (
+            <div className='add-to-do'>
+                <input
+                    onChange={changeDescriptionHandler.bind(this)}
+                    placeholder='Enter a task description'
+                    type='text'
+                    value={this.state.description}
+                />
+                <button
+                    onClick={addToDoHandler.bind(this)}
+                >
+                    { 'Add a To Do' }
+                </button>
+            </div>
+        );
+    }
+}
+
+AddToDo.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    onClick: PropTypes.func.isRequired
+};
---
+++ src/components/AddToDo.spec.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import { AddToDo } from './AddToDo';
+
+const sandbox = sinon.sandbox.create();
+
+describe('<AddToDo/>', () => {
+    let component;
+    let props;
+
+    function renderComponent(overrides) {
+        props = {
+            description: 'some value',
+            onChange: sandbox.stub(),
+            onClick: sandbox.stub(),
+            ...overrides
+        };
+
+        component = shallow(<AddToDo {...props}/>);
+    }
+
+    beforeEach(() => {
+        renderComponent();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('Given the component renders', () => {
+        it('should have a container element', () => {
+            expect(component.type()).toEqual('div');
+            expect(component.hasClass('add-to-do')).toEqual(true);
+        });
+
+        it('should have an input', () => {
+            const input = component.find('input');
+
+            expect(input.type()).toEqual('input');
+            expect(input.props().type).toEqual('text');
+            expect(input.props().placeholder).toEqual('Enter a task description');
+        });
+
+        it('should have a button', () => {
+            const button = component.find('button');
+
+            expect(button.type()).toEqual('button');
+            expect(button.text()).toEqual('Add a To Do');
+        });
+    });
+
+    describe('Given the description is not blank', () => {
+        describe('when the button is clicked', () => {
+            it('should add a new to do and reset the add to do input', () => {
+                renderComponent();
+
+                const event = {
+                    target: {
+                        value: 'some other value'
+                    }
+                };
+
+                let input = component.find('input');
+                const button = component.find('button');
+
+                input.simulate('change', event);
+
+                button.simulate('click');
+
+                input = component.find('input');
+
+                sinon.assert.calledWithExactly(props.onClick, event.target.value);
+                expect(input.props().value).toEqual('');
+            });
+        });
+    });
+
+    describe('Given the description is blank', () => {
+        describe('when the button is clicked', () => {
+            it('should not add a new to do', () => {
+                renderComponent({
+                    description: ''
+                });
+
+                const button = component.find('button');
+
+                button.simulate('click');
+
+                sinon.assert.notCalled(props.onClick);
+            });
+        });
+    });
+
+    describe('when the input is changed', () => {
+        it('should update the input', () => {
+            renderComponent();
+
+            const event = {
+                target: {
+                    value: 'some other value'
+                }
+            };
+
+            component.simulate('change', event);
+
+        });
+    });
+});
