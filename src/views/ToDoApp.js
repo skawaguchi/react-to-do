50cb5eda2215b2f16af6570d3d1c0936edf81fe5
Stephen Kawaguchi <kawaguchistephen@johndeere.com>
2018-02-10 22:19:33 -0500

    chore: add an applicaton header

--- src/views/ToDoApp.js
+++ src/views/ToDoApp.js
@@ -5,6 +5,7 @@ import { ToDoContainer } from '../components/ToDoContainer';
 export function ToDoApp() {
     return (
         <div className='to-do-app'>
+            <h1>{'To Do List'}</h1>
             <ToDoContainer/>
         </div>
     );
