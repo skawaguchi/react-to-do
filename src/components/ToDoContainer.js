83256193495182509ba35fc0f067fb9dd21b1055
Stephen Kawaguchi <kawaguchistephen@johndeere.com>
2018-02-10 22:01:35 -0500

    refactor: move the to do container methods to be private
    - I wanted to reduce the public api and test surface of that component

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -5,6 +5,73 @@ import uniqid from 'uniqid';
 import { AddToDo } from './AddToDo';
 import { ToDoList } from './ToDoList';
 
+function onAddToDo(description) {
+    const newToDo = {
+        description,
+        id: uniqid(),
+        isDone: false
+    };
+
+    this.setState({
+        list: [
+            newToDo,
+            ...this.state.list
+        ]
+    });
+}
+
+function onDescriptionChange(id, event) {
+    const newDescription = event.target.value;
+
+    const newList = this.state.list.reduce((list, item) => {
+        if (item.id === id) {
+            list.push({
+                ...item,
+                description: newDescription
+            });
+        } else {
+            list.push(item);
+        }
+
+        return list;
+    }, []);
+
+    this.setState({
+        list: newList
+    });
+}
+
+function onDoneChange(id) {
+    const newList = this.state.list.reduce((list, item) => {
+        if (item.id === id) {
+            list.push({
+                ...item,
+                isDone: !item.isDone
+            });
+        } else {
+            list.push(item);
+        }
+
+        return list;
+    }, []);
+
+    this.setState({
+        list: newList
+    });
+}
+
+function onRemove(id) {
+    const newList = this.state.list.filter((item) => {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+
+    this.setState({
+        list: newList
+    });
+}
+
 export class ToDoContainer extends Component {
     constructor(props) {
         super(props);
@@ -14,84 +81,17 @@ export class ToDoContainer extends Component {
         };
     }
 
-    onAddToDo(description) {
-        const newToDo = {
-            description,
-            id: uniqid(),
-            isDone: false
-        };
-
-        this.setState({
-            list: [
-                newToDo,
-                ...this.state.list
-            ]
-        });
-    }
-
-    onDescriptionChange(id, event) {
-        const newDescription = event.target.value;
-
-        const newList = this.state.list.reduce((list, item) => {
-            if (item.id === id) {
-                list.push({
-                    ...item,
-                    description: newDescription
-                });
-            } else {
-                list.push(item);
-            }
-
-            return list;
-        }, []);
-
-        this.setState({
-            list: newList
-        });
-    }
-
-    onDoneChange(id) {
-        const newList = this.state.list.reduce((list, item) => {
-            if (item.id === id) {
-                list.push({
-                    ...item,
-                    isDone: !item.isDone
-                });
-            } else {
-                list.push(item);
-            }
-
-            return list;
-        }, []);
-
-        this.setState({
-            list: newList
-        });
-    }
-
-    onRemove(id) {
-        const newList = this.state.list.filter((item) => {
-            if (item.id !== id) {
-                return item;
-            }
-        });
-
-        this.setState({
-            list: newList
-        });
-    }
-
     render() {
         return (
             <main className='to-do-container'>
                 <AddToDo
-                    onAddToDo={this.onAddToDo.bind(this)}
+                    onAddToDo={onAddToDo.bind(this)}
                 />
                 <ToDoList
                     list={this.state.list}
-                    onDescriptionChange={this.onDescriptionChange.bind(this)}
-                    onDoneChange={this.onDoneChange.bind(this)}
-                    onRemove={this.onRemove.bind(this)}
+                    onDescriptionChange={onDescriptionChange.bind(this)}
+                    onDoneChange={onDoneChange.bind(this)}
+                    onRemove={onRemove.bind(this)}
                 />
             </main>
         );
